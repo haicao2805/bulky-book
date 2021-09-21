f3715cbb4a2fd2d6897f6b5047bab5dc81c915f9
Cao Chi Hai <haicao2805@gmail.com>
2021-09-21 20:39:26 +0700

    Done CRUD category

---
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,96 @@
+﻿using BulkyBook.DataAccess.Repository.Interface;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id == 0)
+            {
+                return View(category);
+            }
+
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.CategoryId == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        #region API Call
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allCategories = _unitOfWork.Category.GetAll();
+            return Json(new
+            {
+                data = allCategories
+            });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var obj = _unitOfWork.Category.Get(id);
+            if (obj == null)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Error while deleting"
+                });
+            }
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
+            return Json(new
+            {
+                success = true,
+                message = "Delete successful"
+            });
+        }
+        #endregion
+    }
+}
