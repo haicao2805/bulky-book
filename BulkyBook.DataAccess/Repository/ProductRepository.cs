fd8332c54351690d031c3932cae54b6411d7d87b
Cao Chi Hai <haicao2805@gmail.com>
2021-09-23 23:08:21 +0700

    Update CI/CD

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.Interface;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+            DbSet = _db.Product;
+        }
+        public void Update(Product product)
+        {
+            var obj = DbSet.FirstOrDefault(item => item.Id == product.Id);
+            if (obj != null)
+            {
+                if (product.ImageUrl != null)
+                {
+                    obj.ImageUrl = product.ImageUrl;
+                }
+                obj.Title = product.Title;
+                obj.Description = product.Description;
+                obj.ISBN = product.ISBN;
+                obj.Author = product.Author;
+                obj.Price = product.Price;
+                obj.Price50 = product.Price50;
+                obj.Price100 = product.Price100;
+                obj.ListPrice = product.ListPrice;
+                obj.CategoryId = product.CategoryId;
+                obj.CoverTypeId = product.CoverTypeId;
+
+                _db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,115 @@
+﻿using BulkyBook.DataAccess.Repository.Interface;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _env;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment env)
+        {
+            _unitOfWork = unitOfWork;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            ProductViewModel productViewModel = new ProductViewModel
+            {
+                Product = new Product(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(category => new SelectListItem
+                {
+                    Text = category.Name,
+                    Value = category.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(coverType => new SelectListItem
+                {
+                    Text = coverType.Name,
+                    Value = coverType.Id.ToString()
+                })
+            };
+
+            if (id == null)
+            {
+                return View(productViewModel);
+            }
+
+            productViewModel.Product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+            if (productViewModel.Product == null)
+            {
+                return NotFound();
+            }
+
+            return View(productViewModel);
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Upsert(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (product.Id == 0)
+        //        {
+        //            _unitOfWork.Product.Add(product);
+        //        }
+        //        else
+        //        {
+        //            _unitOfWork.Product.Update(product);
+        //        }
+        //        _unitOfWork.Save();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    return View(product);
+        //}
+
+        #region API Call
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allProducts = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return Json(new
+            {
+                data = allProducts
+            });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var obj = _unitOfWork.Product.Get(id);
+            if (obj == null)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Error while deleting"
+                });
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            return Json(new
+            {
+                success = true,
+                message = "Delete successful"
+            });
+        }
+        #endregion
+    }
+}
